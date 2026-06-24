4384ffc31072177d1d2452c3f83b36bf2b308f3f
Saif Ullah <ranasaifullahkhalid2@gmail.com>
2026-06-24 09:34:59 +0500

    adding 2nd day of code and update privoius

---
+++ day_02_variables.py
@@ -0,0 +1,55 @@
+# String Variable
+city = "Lahore"
+
+# Integer Variable
+population = 13000000
+
+# Float Variable
+temperature = 35.5
+
+# Boolean Variable
+is_capital = False
+
+# Print variables only
+print(city)
+print(population)
+print(temperature)
+print(is_capital)
+
+# ------------------------------------------------ #
+
+
+# Student Information
+
+student_name = "Ahmed"
+semester = 4
+cgpa = 3.45
+is_enrolled = True
+
+# Print with information
+print("Student Name:", student_name)
+print("Semester:", semester)
+print("CGPA:", cgpa)
+print("Currently Enrolled:", is_enrolled)
+
+# ------------------------------------------------ #
+
+
+# Product Information
+
+product_name = "Laptop"
+price = 85000.50
+quantity = 10
+in_stock = True
+
+# Print information
+print("Product Name:", product_name)
+print("Price:", price)
+print("Quantity:", quantity)
+print("In Stock:", in_stock)
+
+# Print data types
+print("Name of product",product_name,"Type of Product Name:", type(product_name))
+print("price of product",price,"Type of Price:", type(price))
+print("Quantity of product",quantity,"Type of Quantity:", type(quantity))
+print("stock available of product",in_stock,"Type of In Stock:", type(in_stock))
