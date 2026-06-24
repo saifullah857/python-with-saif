63fff8acd7be3d509e27f0223b60bf918b59aaf2
Saif Ullah <ranasaifullahkhalid2@gmail.com>
2026-06-24 12:26:04 +0500

    type conversion in python

---
+++ day_03_type_conversion.py
@@ -0,0 +1,89 @@
+# Convert string to integer
+
+age = "20"
+
+print(age)
+print(type(age))
+
+age = int(age)
+
+print(age)
+print(type(age))
+
+# ----------------------------------------
+
+# Convert integer to float
+
+marks = 85
+
+print(marks)
+print(type(marks))
+
+marks = float(marks)
+
+print(marks)
+print(type(marks))
+
+
+# ----------------------------------------
+
+
+
+# Convert float to integer
+
+price = 99.99
+
+print(price)
+print(type(price))
+
+price = int(price)
+
+print(price)
+print(type(price))
+
+# ----------------------------------------
+
+
+# Convert integer to string
+
+roll_no = 101
+
+roll_no = str(roll_no)
+
+print("Roll Number: " + roll_no)
+print(type(roll_no))
+
+
+
+# ----------------------------------------
+
+
+
+# Convert float to string
+
+cgpa = 3.75
+
+cgpa = str(cgpa)
+
+print("CGPA = " + cgpa)
+print(type(cgpa))
+
+# ----------------------------------------
+
+
+# Multiple conversions
+
+value = "45.8"
+
+print(value)
+print(type(value))
+
+value = float(value)
+
+print(value)
+print(type(value))
+
+value = int(value)
+
+print(value)
+print(type(value))
