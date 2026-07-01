0108126fa376223fedf39a72d97f812c685234e7
Saif Ullah <ranasaifullahkhalid2@gmail.com>
2026-07-01 08:40:40 +0500

    Arhmatic operators in python

---
+++ day_04_Arthamatic_operators.py
@@ -0,0 +1,101 @@
+# Add two numbers
+
+num1 = 15
+num2 = 25
+
+result = num1 + num2
+
+print("First Number :", num1)
+print("Second Number:", num2)
+print("Addition =", result)
+
+
+
+# =========================================== #
+
+
+# Remaining salary after expenses
+
+salary = 60000
+expenses = 18500
+
+remaining = salary - expenses
+
+print("Salary            :", salary)
+print("Expenses          :", expenses)
+print("Remaining Salary  :", remaining)
+
+
+
+# =========================================== #
+
+# Area of a rectangle
+
+length = 15
+width = 8
+
+area = length * width
+
+print("Length :", length)
+print("Width  :", width)
+print("Area   :", area)
+
+
+
+
+
+# =========================================== #
+
+
+# Average marks
+
+total_marks = 450
+subjects = 5
+
+average = total_marks / subjects
+
+print("Total Marks :", total_marks)
+print("Subjects    :", subjects)
+print("Average     :", average)
+
+
+
+
+
+# =========================================== #
+
+
+# Remaining money after equal distribution
+
+money = 10000
+friends = 6
+
+remaining_money = money % friends
+
+print("Total Money    :", money)
+print("Friends        :", friends)
+print("Remaining Money:", remaining_money)
+
+# =========================================== #
+
+
+
+# Square of a number
+
+number = 7
+
+square = number ** 2
+
+print("Number :", number)
+print("Square :", square)
+
+
+# Cube of a number
+
+number = 5
+
+cube = number ** 3
+
+print("Number :", number)
+print("Cube   :", cube)
+
