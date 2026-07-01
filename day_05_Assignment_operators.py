2f8328c144831d71df46c0c9371aead04e87359b
Saif Ullah <ranasaifullahkhalid2@gmail.com>
2026-07-01 11:05:20 +0500

    assignment operators

---
+++ day_05_Assignment_operators.py
@@ -0,0 +1,72 @@
+# Assign a value to a variable
+
+number = 20
+
+print("Number =", number)
+
+
+
+# Increase a number
+number = 15
+
+print("Before Addition =", number)
+
+number += 10
+
+print("After Adding 10 =", number)  
+
+
+# Subtract marks
+
+marks = 90
+
+print("Original Marks =", marks)
+
+marks -= 10
+
+print("Marks After Deduction =", marks)
+
+
+# Double a number
+
+number = 12
+
+print("Original Number =", number)
+
+number *= 2
+
+print("After Multiplication =", number)
+
+
+# Divide a number
+
+number = 100
+
+print("Original Number =", number)
+
+number /= 5
+
+print("After Division =", number)
+
+
+
+# Find remainder
+
+number = 25
+
+print("Original Number =", number)
+
+number %= 4
+
+print("Remainder =", number)
+
+
+# Square a number
+
+number = 5
+
+print("Original Number =", number)
+
+number **= 2
+
+print("Square =", number)
