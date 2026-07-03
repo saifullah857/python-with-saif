37114c1d6d73d1f1921d2abb93905fbb9876ad3a
Saif Ullah <ranasaifullahkhalid2@gmail.com>
2026-07-03 12:53:47 +0500

    Relational Operators

---
+++ day_05_Relational_operators.py
@@ -0,0 +1,51 @@
+# Equal To (==)
+
+print(10 == 10)      # True
+print(5 == 8)        # False
+print("AI" == "AI")  # True
+# Not Equal To (!=)
+
+print(10 != 5)        # True
+print(7 != 7)         # False
+print("Python" != "Java")  # True
+# Greater Than (>)
+
+print(15 > 10)    # True
+print(4 > 9)      # False
+print(20 > 20)    # False
+# Less Than (<)
+
+print(3 < 8)      # True
+print(12 < 5)     # False
+print(7 < 7)      # False
+# Greater Than or Equal To (>=)
+
+print(10 >= 10)   # True
+print(15 >= 10)   # True
+print(5 >= 8)     # False
+# Less Than or Equal To (<=)
+
+print(5 <= 5)     # True
+print(3 <= 7)     # True
+print(10 <= 2)    # False
+
+# Combined Example
+
+print("Relational Operators Examples")
+
+print(10 == 10)     # True
+print(20 != 10)     # True
+print(15 > 5)       # True
+print(3 < 8)        # True
+print(7 >= 7)       # True
+print(4 <= 2)       # False
+
+# Relational Operators in Python
+
+# Operator	    Meaning     	            Example
+# ==	            Equal to	                (10 == 10)
+# !=	            Not equal to	            print(5 != 3)
+# >	            Greater than	            print(9 > 4)
+# <	            Less than	                print(2 < 8)
+# >=	            Greater than or equal to	print(7 >= 7)
+# <=	        Less than or equal to	        print(5 <= 10)
