ca82e9dc9b903718f5a5d5c5bc08c975dc1677c6
Saif Ullah <ranasaifullahkhalid2@gmail.com>
2026-07-21 09:48:49 +0500

    day 6 assignment operators

---
+++ day_06_Assignment_operators.py
@@ -0,0 +1,67 @@
+# Assign Value
+
+num = 10
+print(num)
+# Add and Assign
+
+num = 10
+num += 5
+print(num)
+# Subtract and Assign
+
+num = 20
+num -= 8
+print(num)
+# Multiply and Assign
+
+num = 6
+num *= 4
+print(num)
+# Divide and Assign
+
+num = 20
+num /= 5
+print(num)
+# Floor Divide and Assign
+
+num = 22
+num //= 5
+print(num)
+# Modulus and Assign
+
+num = 17
+num %= 5
+print(num)
+
+# Exponent and Assign
+
+num = 3
+num **= 3
+
+# Combined Example
+num = 10
+print("Initial Value:", num)
+
+num += 5
+print("After += :", num)
+
+num -= 3
+print("After -= :", num)
+
+num *= 2
+print("After *= :", num)
+
+num /= 4
+print("After /= :", num)
+
+num %= 3
+print("After %= :", num)
+# Assignment Operators Summary
+# Operator	 Meaning	            Example
+# =	        Assign	                x = 10
+# +=	        Add and assign	        x += 5
+# -=	        Subtract and assign	    x -= 2
+# *=	        Multiply and assign	    x *= 3
+# /=	        Divide and assign	    x /= 2
+# %=	        Modulus and assign	    x %= 4
+# **=	        Exponent and assign	    x **= 2
